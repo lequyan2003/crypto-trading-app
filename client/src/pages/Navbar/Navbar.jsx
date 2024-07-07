2c324dc066cede11db8a75cd0c1062398b87dde7
lequyan2003 <lequyan2003@gmail.com>
2024-07-06 22:57:16 -0400

    avatar added

--- client/src/pages/Navbar/Navbar.jsx
+++ client/src/pages/Navbar/Navbar.jsx
@@ -1,5 +1,55 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { DragHandleHorizontalIcon } from "@radix-ui/react-icons";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div
+      className="
+        px-2 py-3 border-b z-50 bg-background bg-opacity-0 sticky
+        top-0 left-0 right-0 flex justify-between items-center
+    "
+    >
+      <div className="flex items-center gap-3">
+        <Sheet>
+          <SheetTrigger>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="rounded-full h-11 w-11"
+            >
+              <DragHandleHorizontalIcon className="h-7 w-7" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent
+            className="w-72 border-r-0 flex flex-col justify-center"
+            side="left"
+          >
+            <SheetHeader>
+              <SheetTitle>
+                <div className="text-3xl flex justify-center items-center gap-1">
+                  <Avatar>
+                    <AvatarImage src="https://cdn.pixabay.com/photo/2021/05/09/13/10/finance-6240949_1280.png" />
+                  </Avatar>
+                  <div>
+                    <span className="font-bold text-orange-700">Trek</span>
+                    <span>Trade</span>
+                  </div>
+                </div>
+              </SheetTitle>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
