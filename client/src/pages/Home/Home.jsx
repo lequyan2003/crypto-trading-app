34fd8a55c295909094b57ec33b503a600630423b
lequyan2003 <lequyan2003@gmail.com>
2024-07-10 22:05:38 -0400

    chat box upgraded, home page finished

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Cross1Icon, DotIcon } from "@radix-ui/react-icons";
 import { MessageCircle } from "lucide-react";
 import { useState } from "react";
@@ -8,9 +9,26 @@ import StockChart from "./StockChart";
 
 const Home = () => {
   const [category, setCategory] = useState("all");
+  const [inputValue, setInputValue] = useState("");
+  const [isBotRelease, setIsBotRelease] = useState(false);
+
+  const handleBotRelease = () => {
+    setIsBotRelease(!isBotRelease);
+  };
 
   const handleCategory = (value) => {
     setCategory(value);
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key == "Enter") {
+      console.log(inputValue);
+    }
+    setInputValue("");
   };
   return (
     <div className="relative">
@@ -82,19 +100,65 @@ const Home = () => {
           absolute bottom-5 right-5 z-40 flex flex-col justify-end items-end gap-2
         "
       >
-        <div
-          className="
-            rounded-md w-[20rem] md:w-[25rem] lg:w-[25rem] h-[70vh] bg-slate-900"
-        >
-          <div className="flex justify-between items-center border-b px-6 h-[12%]">
-            <p>Chat Bot</p>
-            <Button variant="ghost" size="icon">
-              <Cross1Icon />
-            </Button>
+        {isBotRelease && (
+          <div
+            className="
+            rounded-md w-[20rem] md:w-[25rem] lg:w-[25rem] h-[70vh] bg-slate-900
+          "
+          >
+            <div className="flex justify-between items-center border-b px-6 h-[12%]">
+              <p>Chat Bot</p>
+              <Button onClick={handleBotRelease} variant="ghost" size="icon">
+                <Cross1Icon />
+              </Button>
+            </div>
+
+            <div className="h-[76%] flex flex-col overflow-y-auto gap-5 px-5 py-2 scroll-container">
+              <div className="self-start pb-5 w-auto">
+                <div className="justify-end self-end px-5 py-2 rounded-md bg-slate-800 w-auto">
+                  <p>Hi, Abel Le</p>
+                  <p>You can ask any questions related to crypto</p>
+                  <p>like price, market cap,...</p>
+                </div>
+              </div>
+
+              {[1, 1, 1, 1].map((item, i) => (
+                <div
+                  key={i}
+                  className={` ${
+                    i % 2 == 0 ? "self-start" : "self-end"
+                  } "pb-5 w-auto" `}
+                >
+                  {i % 2 == 0 ? (
+                    <div className="justify-end self-end px-5 py-2 rounded-md bg-slate-800 w-auto">
+                      <p>Prompt: Who are you</p>
+                    </div>
+                  ) : (
+                    <div className="justify-end self-end px-5 py-2 rounded-md bg-slate-800 w-auto">
+                      <p>Answer: Hi, I am Abel Le</p>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+
+            <div className="h-[12%] border-t ">
+              <Input
+                className="w-full h-full order-none outline-none"
+                placeholder="write something"
+                onChange={handleChange}
+                value={inputValue}
+                onKeyPress={handleKeyPress}
+              />
+            </div>
           </div>
-        </div>
+        )}
+
         <div className="relative w-[10rem] cursor-pointer group">
-          <Button className="w-full h-[3rem] gap-2 items-center">
+          <Button
+            onClick={handleBotRelease}
+            className="w-full h-[3rem] gap-2 items-center"
+          >
             <MessageCircle
               size={30}
               className="fill-[#1e293b] -rotate-90 stroke-none group-hover:fill-[#1a1a1a]"
