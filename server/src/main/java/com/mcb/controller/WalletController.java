285f4ca8d1033d9c384b4fbd0bf49c409774d776
lequyan2003 <lequyan2003@gmail.com>
2024-07-24 19:02:54 -0400

    wallet api added _ 2, waiting for adding order api

--- server/src/main/java/com/mcb/controller/WalletController.java
+++ server/src/main/java/com/mcb/controller/WalletController.java
@@ -1,5 +1,74 @@
 package com.mcb.controller;
 
-public class WalletController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.mcb.modal.Order;
+import com.mcb.modal.User;
+import com.mcb.modal.Wallet;
+import com.mcb.modal.WalletTransaction;
+import com.mcb.service.UserService;
+import com.mcb.service.WalletService;
+
+
+
+@RestController
+@RequestMapping("/api/wallet")
+public class WalletController {
+    @Autowired
+    private WalletService walletService;
+
+    @Autowired
+    private UserService userService;
+
+    private OrderService
+
+    @GetMapping("/api/wallet")
+    public ResponseEntity<Wallet> getUserWallet(
+        @RequestHeader("Authorization") String jwt
+    ) {
+        User user = userService.findUserProfileByJwt(jwt);
+
+        Wallet wallet = walletService.getUserWallet(user);
+
+        return new ResponseEntity<>(wallet, HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/api/wallet/{walletId}/transfer")
+    public ResponseEntity<Wallet> walletToWalletTransfer(
+        @RequestHeader("Authorization") String jwt,
+        @PathVariable Long walletId,
+        @RequestBody WalletTransaction req
+    ) throws Exception {
+        User senderUser = userService.findUserProfileByJwt(jwt);
+        Wallet receiverWallet = walletService.findWalletById(walletId);
+        Wallet wallet = walletService.walletToWalletTransfer(
+            senderUser,
+            receiverWallet,
+            req.getAmount()
+        );
+
+        return new ResponseEntity<>(wallet, HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/api/wallet/order/{orderId}/pay")
+    public ResponseEntity<Wallet> payOrderPayment(
+        @RequestHeader("Authorization") String jwt,
+        @PathVariable Long orderId
+    ) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        
+        Order order = orderService.getOrderById(orderId);
+
+        Wallet wallet = walletService.payOrderPayment(order, user);
+
+        return new ResponseEntity<>(wallet, HttpStatus.ACCEPTED);
+    }
 }
---
+++ server/src/main/java/com/mcb/domain/WalletTransactionType.java
@@ -0,0 +1,9 @@
+package com.mcb.domain;
+
+public enum WalletTransactionType {
+    WITHDRAWAL,
+    WALLET_TRANSFER,
+    ADD_MONEY,
+    BUY_ASSET,
+    SELL_ASSET
+}
