f9331c97c0183624d2b99a5f8f31ea48ff53902a
lequyan2003 <lequyan2003@gmail.com>
2024-07-30 19:58:09 -0400

    payment details added

--- client/src/pages/Payment Details/PaymentDetails.jsx
+++ client/src/pages/Payment Details/PaymentDetails.jsx
@@ -1,5 +1,57 @@
+/* eslint-disable no-constant-condition */
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import PaymentDetailsForm from "./PaymentDetailsForm";
+
 const PaymentDetails = () => {
-  return <div>PaymentDetails</div>;
+  return (
+    <div className="px-20">
+      <h1 className="text-3xl font-bold py-10">Payment Details</h1>
+      {false ? (
+        <Card>
+          <CardHeader>
+            <CardTitle>Yes Bank</CardTitle>
+            <CardDescription>A/C No : ********1563</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center">
+              <p className="w-32">A/C Holder</p>
+              <p className="text-gray-400"> : Abel</p>
+            </div>
+            <div className="flex items-center">
+              <p className="w-32">IFSC</p>
+              <p className="text-gray-400"> : YESA00000007</p>
+            </div>
+          </CardContent>
+        </Card>
+      ) : (
+        <Dialog>
+          <DialogTrigger>
+            <Button className="py-6">Add Payment Details</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Payment Details</DialogTitle>
+            </DialogHeader>
+            <PaymentDetailsForm />
+          </DialogContent>
+        </Dialog>
+      )}
+    </div>
+  );
 };
 
 export default PaymentDetails;
---
+++ client/src/pages/Payment Details/PaymentDetailsForm.jsx
@@ -0,0 +1,134 @@
+import { Button } from "@/components/ui/button";
+import { DialogClose } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+
+const PaymentDetailsForm = () => {
+  const form = useForm({
+    resolver: "",
+    defaultValues: {
+      accountHolderName: "",
+      ifsc: "",
+      accountNumber: "",
+      bankName: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="px-10 py-2">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="accountHolderName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Account Holder Name</FormLabel>
+                <FormControl>
+                  <Input
+                    className="border w-full border-gray-700 p-5"
+                    placeholder="Abel"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="ifsc"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>IFSC Code</FormLabel>
+                <FormControl>
+                  <Input
+                    className="border w-full border-gray-700 p-5"
+                    placeholder="Abel"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="accountNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Account Number</FormLabel>
+                <FormControl>
+                  <Input
+                    className="border w-full border-gray-700 p-5"
+                    placeholder="********5694"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="confirmAccountNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm Account Number</FormLabel>
+                <FormControl>
+                  <Input
+                    className="border w-full border-gray-700 p-5"
+                    placeholder="confirm account number"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="bankName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Bank Name</FormLabel>
+                <FormControl>
+                  <Input
+                    className="border w-full border-gray-700 p-5"
+                    placeholder="Yes Bank"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <DialogClose className="w-full">
+            <Button type="submit" className="w-full py-5">
+              Submit
+            </Button>
+          </DialogClose>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default PaymentDetailsForm;
