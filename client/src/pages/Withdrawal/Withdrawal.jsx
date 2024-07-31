46c088114d7d54d0420e04aa0f8d2eea005f1a73
lequyan2003 <lequyan2003@gmail.com>
2024-07-31 13:22:36 -0400

    withdrawal page added

--- client/src/pages/Withdrawal/Withdrawal.jsx
+++ client/src/pages/Withdrawal/Withdrawal.jsx
@@ -1,5 +1,40 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 const Withdrawal = () => {
-  return <div>Withdrawal</div>;
+  return (
+    <div className="p-5 lg:px-20">
+      <h1 className="font-bold text-3xl pb-5">Withdrawal</h1>
+      <Table className="border">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="py-5">Date</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead>Amount</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item, index) => (
+            <TableRow key={index}>
+              <TableCell>
+                <p>June 2, 2024 at 11:43</p>
+              </TableCell>
+              <TableCell className="">Bank</TableCell>
+              <TableCell className="">$69249</TableCell>
+              <TableCell className="text-right">345</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 export default Withdrawal;
