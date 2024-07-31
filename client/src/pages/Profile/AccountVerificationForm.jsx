b2fcd7d37e591a14792ecec895e674791bed19da
lequyan2003 <lequyan2003@gmail.com>
2024-07-31 13:57:31 -0400

    profile page added

---
+++ client/src/pages/Profile/AccountVerificationForm.jsx
@@ -0,0 +1,71 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { useState } from "react";
+
+const AccountVerificationForm = () => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    console.log(value);
+  };
+
+  return (
+    <div className="flex justify-center">
+      <div className="space-y-5 mt-10 w-full">
+        <div className="flex justify-between items-center">
+          <p>Email :</p>
+          <p>abel@gmail.com</p>
+          <Dialog>
+            <DialogTrigger>
+              <Button>Send OTP</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Enter OTP</DialogTitle>
+              </DialogHeader>
+              <div className="py-5 flex gap-10 justify-center items-center">
+                <InputOTP
+                  value={value}
+                  onChange={(value) => setValue(value)}
+                  maxLength={6}
+                >
+                  <InputOTPGroup>
+                    <InputOTPSlot index={0} />
+                    <InputOTPSlot index={1} />
+                    <InputOTPSlot index={2} />
+                  </InputOTPGroup>
+                  <InputOTPSeparator />
+                  <InputOTPGroup>
+                    <InputOTPSlot index={3} />
+                    <InputOTPSlot index={4} />
+                    <InputOTPSlot index={5} />
+                  </InputOTPGroup>
+                </InputOTP>
+                <DialogClose>
+                  <Button onClick={handleSubmit} className={"w-[10rem]"}>
+                    Submit
+                  </Button>
+                </DialogClose>
+              </div>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountVerificationForm;
--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,5 +1,115 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { VerifiedIcon } from "lucide-react";
+import AccountVerificationForm from "./AccountVerificationForm";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const handleEnableTwoStepVerification = () => {
+    console.log("two step verification");
+  };
+
+  return (
+    <div className="flex flex-col items-center mb-5">
+      <div className="pt-10 w-full lg:w-[60%]">
+        <Card>
+          <CardHeader className="pb-9">
+            <CardTitle>Your Information</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="lg:flex gap-32">
+              <div className="space-y-7">
+                <div className="flex">
+                  <p className="w-[9rem]">Email : </p>
+                  <p className="text-gray-500">abel@gmail.com</p>
+                </div>
+
+                <div className="flex">
+                  <p className="w-[9rem]">Full Name : </p>
+                  <p className="text-gray-500">Abel Le</p>
+                </div>
+
+                <div className="flex">
+                  <p className="w-[9rem]">Date of Birth : </p>
+                  <p className="text-gray-500">02/25/2003</p>
+                </div>
+
+                <div className="flex">
+                  <p className="w-[9rem]">Nationality : </p>
+                  <p className="text-gray-500">Vietnamese</p>
+                </div>
+              </div>
+
+              <div className="space-y-7">
+                <div className="flex">
+                  <p className="w-[9rem]">Email : </p>
+                  <p className="text-gray-500">abel@gmail.com</p>
+                </div>
+
+                <div className="flex">
+                  <p className="w-[9rem]">Full Name : </p>
+                  <p className="text-gray-500">Abel Le</p>
+                </div>
+
+                <div className="flex">
+                  <p className="w-[9rem]">Date of Birth : </p>
+                  <p className="text-gray-500">02/25/2003</p>
+                </div>
+
+                <div className="flex">
+                  <p className="w-[9rem]">Nationality : </p>
+                  <p className="text-gray-500">Vietnamese</p>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+
+        <div className="mt-6">
+          <Card className="w-full">
+            <CardHeader className="pb-7">
+              <div className="flex items-center gap-3">
+                <CardTitle>2 Step Verification</CardTitle>
+
+                {true ? (
+                  <Badge className={"space-x-2 text-white bg-green-600"}>
+                    <VerifiedIcon />
+                    <span>Enabled</span>
+                  </Badge>
+                ) : (
+                  <Badge className="bg-orange-500">Disabled</Badge>
+                )}
+              </div>
+            </CardHeader>
+            <CardContent>
+              <div>
+                <Dialog>
+                  <DialogTrigger>
+                    <Button>Enable Two Step Verification</Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Verify your account</DialogTitle>
+                    </DialogHeader>
+                    <AccountVerificationForm
+                      handleSubmit={handleEnableTwoStepVerification}
+                    />
+                  </DialogContent>
+                </Dialog>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
