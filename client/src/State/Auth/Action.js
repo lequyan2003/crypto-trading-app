2350304127c6335153f57e294206e5e75300a921
lequyan2003 <lequyan2003@gmail.com>
2024-08-08 22:16:14 -0400

    connect_1_comeback_after_1_week_break_axios and redux installed and start to implement

---
+++ client/src/State/Auth/Action.js
@@ -0,0 +1,57 @@
+import axios from "axios"
+import { GET_USER_FAILURE, GET_USER_REQUEST, GET_USER_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS } from "./ActionTypes";
+
+export const register = (userData) => async(dispatch) => {
+    dispatch({ type: REGISTER_REQUEST });
+
+    const baseUrl = "http://localhost:5454";
+
+    try {
+        const response = await axios.post(`${baseUrl}/auth/signup`, userData);
+        const user = response.data;
+        console.log(user);
+
+        dispatch({ type: REGISTER_SUCCESS, payload: user.jwt });
+    } catch (error) {
+        dispatch({ type: REGISTER_FAILURE, payload: error.message });
+        console.log(error);
+    }
+}
+
+export const login = (userData) => async(dispatch) => {
+    dispatch({ type: LOGIN_REQUEST });
+
+    const baseUrl = "http://localhost:5454";
+
+    try {
+        const response = await axios.post(`${baseUrl}/auth/signin`, userData);
+        const user = response.data;
+        console.log(user);
+
+        dispatch({ type: LOGIN_SUCCESS, payload: user.jwt });
+    } catch (error) {
+        dispatch({ type: LOGIN_FAILURE, payload: error.message });
+        console.log(error);
+    }
+}
+
+export const getUser = (jwt) => async(dispatch) => {
+    dispatch({ type: GET_USER_REQUEST });
+
+    const baseUrl = "http://localhost:5454";
+
+    try {
+        const response = await axios.get(`${baseUrl}/api/users/profile`, {
+            headers: {
+                Authorization: `Bearer ${jwt}`
+            }
+        });
+        const user = response.data;
+        console.log(user);
+
+        dispatch({ type: GET_USER_SUCCESS, payload: user });
+    } catch (error) {
+        dispatch({ type: GET_USER_FAILURE, payload: error.message });
+        console.log(error);
+    }
+}
